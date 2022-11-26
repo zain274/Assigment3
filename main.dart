b714c44435d79a9580313a6d1518c37ff33b4d88
Zain Raza <zrabbas110@gmail.com>
2022-11-27 01:04:46 +0500

    Assigment complete

---
+++ main.dart
@@ -0,0 +1,115 @@
+void main(List<String> args) {
+  
+  //Question 1
+
+  // List name=["Muntazir","AliRaza","Kazim","Ali","Mehdi","Hasan","Salman"];
+  // print(name);
+
+
+
+//Question 2
+
+// List <String> days=[];
+// days.addAll(["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]);
+// print(days);
+
+
+//Question 3
+
+
+//List names=["Muntazir","AliRaza","Kazim","Ali","Mehdi","Hasan","Salman"];
+   //print(names.singleWhere((i) => i[0] == "a"));
+
+
+// Question 4
+//       Map details = {
+//     'name' : "Zain Raza",
+//      'address' : "Karachi",
+//      'age' : 18 ,
+//      'country' : 'Pakistan'
+//  };
+
+//   details['country'] = "Iran" ;
+
+//    print(details);
+
+
+
+
+//Question 5
+
+
+
+//   Map details = {
+//     'name' : 'ZainRaza',
+//     'phone' : "03101022338" ,
+//   };
+
+
+// details.forEach((key,val){
+
+//   if(key.length ==  5){
+//     print(key);
+//   };
+
+
+
+
+
+
+// });
+
+
+//Question 6
+
+
+
+//   var world = {
+//     'Country' : "Pakistan",
+//     'CapitalCity' : 'Islamabad',
+//     'Currency' : "Rupee" ,
+//     'Language' : 'Urdu',
+//   };
+
+//  world.forEach((key,val){
+//    print(val);
+//  });
+
+
+//Question 7
+
+//   Map<String, double> mathMarks = {
+//    'ram': 30,
+//    'mark': 32,
+//   'harry': 88,
+//   'raj': 69,
+//   'john': 15,
+//  };
+
+//   mathMarks.removeWhere((key,val)=>val <= 30);
+
+//   print(mathMarks);
+
+
+
+
+//Question 8
+
+
+//   QUESTION NO 8
+
+//   Map<String, double> expenses = {
+//   'sun': 3000.0,
+//   'mon': 3000.0,
+//   'tue': 3234.0,
+//  };
+
+//    expenses['fri'] = 5000.0 ;
+
+//    print(expenses);
+
+
+
+
+
+}
